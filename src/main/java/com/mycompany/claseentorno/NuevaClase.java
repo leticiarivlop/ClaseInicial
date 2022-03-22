0cc4a434fa58ffb72fd6e9b2aa94b14876c35e9a
leticiarivlop <98585620+leticiarivlop@users.noreply.github.com>
2022-03-22 08:54:43 +0100

    Create NuevaClase.java

---
+++ src/main/java/com/mycompany/claseentorno/NuevaClase.java
@@ -0,0 +1,7 @@
+public class NuevaClase {
+    public static void main(String[] args) {
+        System.out.println("Hola Netbeans");
+           System.out.println("Nuevo Proyecto Creado");
+    }
+    
+}
